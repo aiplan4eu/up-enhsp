0ae1aea9e7cac73800e2a4491345dda540312bc6
Alessandro Valentini <alvalentini@fbk.eu>
2022-06-13 16:56:05 +0200

    Update the code according to the new UP changes

--- up_enhsp/enhsp_planner.py
+++ up_enhsp/enhsp_planner.py
@@ -1,28 +1,29 @@
 import pkg_resources # type: ignore
 import unified_planning as up
-from unified_planning.solvers.results import PlanGenerationResultStatus
+from unified_planning.engines.results import PlanGenerationResultStatus
 from unified_planning.model import ProblemKind
-from unified_planning.solvers import PDDLSolver
+from unified_planning.engines import PDDLPlanner, Credits
 from typing import Optional, List, Union
 
 
-class ENHSPsolver(PDDLSolver):
+credits = Credits('ENHSP',
+                  'Enrico Scala',
+                  'enricos83@gmail.com',
+                  'https://sites.google.com/view/enhsp/',
+                  'GPL',
+                  'Expressive Numeric Heuristic Search Planner.',
+                  'ENHSP is an expressive classical, linear and non linear numeric planner.')
+
+class ENHSPEngine(PDDLPlanner):
 
     def __init__(self, search_algorithm: Optional[str] = None, heuristic: Optional[str] = None):
         super().__init__(needs_requirements=False)
         self.search_algorithm = search_algorithm
         self.heuristic = heuristic
 
-    def destroy(self):
-        pass
-
     @property
     def name(self) -> str:
         return 'enhsp'
-
-    @staticmethod
-    def is_oneshot_planner() -> bool:
-        return True
 
     def _manage_parameters(self, command):
         if self.search_algorithm is not None:
@@ -41,7 +42,7 @@ class ENHSPsolver(PDDLSolver):
         return PlanGenerationResultStatus.UNSOLVABLE_PROVEN if plan is None else PlanGenerationResultStatus.SOLVED_SATISFICING
 
     @staticmethod
-    def supports(problem_kind: 'ProblemKind') -> bool:
+    def supported_kind() -> bool:
         supported_kind = ProblemKind()
         supported_kind.set_numbers('CONTINUOUS_NUMBERS')  # type: ignore
         supported_kind.set_numbers('DISCRETE_NUMBERS')  # type: ignore
@@ -55,10 +56,18 @@ class ENHSPsolver(PDDLSolver):
         supported_kind.set_effects_kind('INCREASE_EFFECTS')  # type: ignore
         supported_kind.set_effects_kind('DECREASE_EFFECTS')  # type: ignore
         supported_kind.set_effects_kind('CONDITIONAL_EFFECTS')  # type: ignore
-        return problem_kind <= supported_kind
+        return supported_kind
+
+    @staticmethod
+    def supports(problem_kind: 'ProblemKind') -> bool:
+        return problem_kind <= ENHSPEngine.supported_kind()
+
+    @staticmethod
+    def get_credits(**kwargs) -> Optional[up.engines.Credits]:
+        return credits
 
 
-class ENHSPSatSolver(ENHSPsolver):
+class ENHSPSatEngine(ENHSPEngine):
 
     @property
     def name(self) -> str:
@@ -71,7 +80,7 @@ class ENHSPSatSolver(ENHSPsolver):
         return command
 
 
-class ENHSPOptSolver(ENHSPsolver):
+class ENHSPOptEngine(ENHSPEngine):
 
     @property
     def name(self) -> str:
@@ -84,10 +93,10 @@ class ENHSPOptSolver(ENHSPsolver):
         return command
 
     @staticmethod
-    def satisfies(optimality_guarantee: Union['up.solvers.solver.OptimalityGuarantee', str]) -> bool:
+    def satisfies(optimality_guarantee: 'up.engines.engine.OptimalityGuarantee') -> bool:
         return True
 
     def _result_status(self, problem: 'up.model.Problem', plan: Optional['up.plan.Plan']) -> 'PlanGenerationResultStatus':
         '''Takes a problem and a plan and returns the status that represents this plan.
         The possible status with their interpretation can be found in the up.plan file.'''
-        return PlanGenerationResultStatus.UNSOLVABLE_PROVEN if plan is None else up.solvers.results.PlanGenerationResultStatus.SOLVED_OPTIMALLY
+        return PlanGenerationResultStatus.UNSOLVABLE_PROVEN if plan is None else up.engines.results.PlanGenerationResultStatus.SOLVED_OPTIMALLY
