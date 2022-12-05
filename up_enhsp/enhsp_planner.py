e863f0ee34b609bf89f2d661b37050093970e597
Enrico Scala <enrico@Enricos-MacBook-Pro.local>
2022-12-05 14:51:50 +0100

    Fixed a few thing to have ENHSP return the right solved outputs, and supports metric through minimize in simple numeric planning context

--- up_enhsp/enhsp_planner.py
+++ up_enhsp/enhsp_planner.py
@@ -71,6 +71,7 @@ class ENHSPEngine(PDDLPlanner):
         supported_kind.set_effects_kind('CONDITIONAL_EFFECTS')  # type: ignore
         supported_kind.set_quality_metrics("ACTIONS_COST")
         supported_kind.set_quality_metrics("PLAN_LENGTH")
+        supported_kind.set_quality_metrics("FINAL_VALUE")
         return supported_kind
 
     @staticmethod
@@ -134,5 +135,5 @@ class ENHSPOptEngine(ENHSPEngine):
             return PlanGenerationResultStatus.UNSOLVABLE_PROVEN
         else:
             if not problem.quality_metrics:
-                raise Exception('Impossible solve with optimality guarantee without specific metric')
+                return PlanGenerationResultStatus.SOLVED_SATISFICING
             return PlanGenerationResultStatus.SOLVED_OPTIMALLY
