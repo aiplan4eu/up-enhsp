963701090b637adb10dd6a569350ce8aa74b8e17
Enrico Scala <enrico@Enricos-MacBook-Pro-2.local>
2023-07-14 15:51:01 +0200

    Added version

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ class InstallENHSPdevelop(develop):
 
 
 setup(name='up_enhsp',
-      version='0.0.15',
+      version='0.0.16',
       description='up_enhsp',
       author='UNIBS Team',
       author_email='enrico.scala@unibs.it',
